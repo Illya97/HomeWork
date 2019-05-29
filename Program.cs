df1082e0aec6de3f790646a981a6893e0bb7d6b4
Ілля Овсянніков <50924109+Illya97@users.noreply.github.com>
2019-05-29 05:00:05 -0600

    Lesson 3_1
    Lesson 3_1

--- Program.cs
+++ Program.cs
@@ -1,48 +1,40 @@
 ﻿using System;
 
-namespace HomeWork3
+namespace HomeWork3_switch
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Згенерировать число 
-            Random rnd = new Random();
-            int сorrect_number = rnd.Next(0,100);
-            byte count = 1;
-       
-            while (count!=6) {
-                    //Ввод числа пользователем
-                    Console.WriteLine("Try#" + count);
-                    Console.Write("Enter your number from 0 to 100: ");
-                    byte user_number = Convert.ToByte(Console.ReadLine());
-                    Console.Write("");
-                // Проверка на правильность ввода
-                if (user_number < 0 || user_number > 100)
-                    {
-                        Console.WriteLine("Enter correct number.");
-                    continue;
-                    }
-                //Подсказки для пользователя и выход с цикла если пользователь угадал число 
-                if (user_number < сorrect_number)
-                {
-                    Console.WriteLine("Wrong!My number is bigger!");
-                }
-                else if (user_number > сorrect_number)
-                {
-                    Console.WriteLine("Wrong!My number is smaller!");
-                }
-                else
-                {
-                    Console.WriteLine("You win!!!Congratulations, your score:"+ count+";");
+            int discount = 0;
+            Console.WriteLine("Enter total money:");
+            int total = Convert.ToInt32(Console.ReadLine());
+            switch (total >0 && total<300)
+            {
+                case true:
+                    discount = 0;
                     break;
-                }
-                count++;
             }
-            Console.WriteLine("You lost! Try again. My numbe is "+ сorrect_number+".");
-
-
-
+            switch (total >= 300 && total < 400)
+            {
+                case true:
+                    discount = 3;
+                    break;
+            }
+            switch (total >= 400 && total <  500)
+            {
+                case true:
+                    discount = 5;
+                    break;
+            }
+            switch (total >= 500)
+            {
+                case true:
+                    discount = 7;
+                    break;
+            }
+            
+            Console.WriteLine("Скидка:" + discount + "%; Сума к оплате:" + (total - ((total * discount) / 100.00)) + ";");
         }
     }
 }
