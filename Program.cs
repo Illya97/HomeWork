5093a2c95bc4f67fc843e8c2db83a4f780b90913
Ілля Овсянніков <50924109+Illya97@users.noreply.github.com>
2019-06-05 08:27:48 -0600

    Lesson4_3

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Home_Work4_3Easy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            char[] arr = new char[10];
+            arr[0] = '@';
+            for (int i = 1; i < arr.Length; i++)
+            {
+                arr[i] = '_';
+            }
+            //----------------------------------------
+            Boolean finish = true;
+            char temp = ' ';
+            int hp = 100; 
+            do
+            {
+                if (arr[arr.Length - 1] == '@')
+                {
+                    finish = false;
+                    break;
+                }
+                // Вивод масива
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write(arr[i] + "");
+
+                }
+                Console.WriteLine("");
+                Console.WriteLine("Введите действие: a - движение влево, d - движение вправо.");
+                string run = Console.ReadLine();
+                if (run == "a" && arr[0] != '@')
+                {
+                    temp = arr[0];
+                    for (int i = 0; i < arr.Length - 1; i++)
+                    {
+                        arr[i] = arr[i + 1];
+                    }
+                    arr[arr.Length - 1] = temp;
+                    hp -= 5;
+                }
+                else if (run == "d")
+                {
+                    temp = arr[arr.Length - 1];
+                    for (int i = arr.Length - 1; i > 0; i--)
+                    {
+                        arr[i] = arr[i - 1];
+                    }
+                    arr[0] = temp;
+                    hp -= 5;
+                }
+
+            } while (finish && hp > 0);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + "");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Game Over");
+        }
+    }
+}
