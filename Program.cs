3ab1c5147f9b90170997ed3625e0f1de59562340
Ілля Овсянніков <50924109+Illya97@users.noreply.github.com>
2019-06-07 06:00:17 -0600

    Lesson6_2
    Lesson6_2

--- Program.cs
+++ Program.cs
@@ -1,84 +1,44 @@
 ﻿using System;
 
-namespace HomeWork6_1
+namespace HomeWork_6_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Ввод данних
-            Console.WriteLine("Enter last first Surname: ");
-            string lastfirstsurname = Console.ReadLine(); 
+            Random rnd = new Random();
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            int rnd_word = rnd.Next(3,8);
+            int rnd_letter = rnd.Next(5,10);
+            string[] arr_words= new string[rnd_word];
             string tempString = "";
-            Boolean Space = false;
-            // Удаляем ненужные пробелы
-            lastfirstsurname = lastfirstsurname.Trim();
-            for (int i = 0; i < lastfirstsurname.Length; i++)
+            for (int i = 0; i < arr_words.Length; i++)
             {
-                //если пробел..
-                if (lastfirstsurname[i] == ' ')
+                for (int j = 0; j < rnd_letter; j++)
                 {
-                    //если перед этим тоже был пробел..
-                    if (Space)
+                    tempString += chars[rnd.Next(chars.Length)];
+                }
+                arr_words[i] = tempString;
+                tempString += " ";
+            }
+       
+            string arr_consonant = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+            int count = 0;
+            for (int i = 0; i < tempString.Length; i++)
+            {
+                for (int j = 0; j < arr_consonant.Length; j++)
+                {
+                    if (tempString[i] == arr_consonant[j])
                     {
-                        //следующая итерация 
-                        continue;
-                        
+                        count++;
+                    }
+                }
+               
+            }
+            Console.WriteLine("Рандомная строка слов: \n"+tempString+ "\nСогласные буквы в алфавите:\n"+ arr_consonant+ "\nколичество согласних слов:"+count);
 
-                    }
-                    else
-                    {
-                        //если перед этим пробелом небыло устанавливаем флаг первого пробела
-                        Space = true;
-                        tempString += lastfirstsurname[i];
-                    }
-                   
-                }
-                else
-                {
-                    //если символ не пробел то убираем флаг первого пробела
-                    Space = false;
-                    tempString += lastfirstsurname[i];
-                }
-            }
-            lastfirstsurname = tempString;
-            tempString = "";
-            Console.WriteLine(tempString);
-            //Розбивка на слова 
-            string[] words = lastfirstsurname.Split(new char[] { ' ' });
-            lastfirstsurname = "";
-            for (int i = 0; i < words.Length; i++)
-            {
-                if (i==0)
-                {
-                    for (int j = 0; j < words[0].Length; j++)
-                    {
-                        if (j==0)
-                        {
-                            tempString += words[i][j].ToString().ToUpper();
-                        }
-                        else
-                        {
-                            tempString += words[i][j];
-                        }
-                    }
-                    words[i] = tempString;
-                }
-                else if (i == 1)
-                {
-                    words[1] = words[1].ToUpper().Remove(1,words[1].Length-1);
-                    words[1] = words[1] + '.';
-                   
-                }
-                else if (i == 2)
-                {
-                    words[2] = words[2].Remove(1, words[2].Length-1).ToUpper();
-                    words[2]=words[2] + '.';
-                   
-                }
-                lastfirstsurname += words[i]+" ";
-            }
-            Console.WriteLine(lastfirstsurname);
+
+
         }
     }
 }
